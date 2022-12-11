aaaaf063b044efe644d5a0b3b10ef7fe96726556
k0825 <kazukkiyo@icloud.com>
2022-12-11 13:58:14 +0900

    SNSにアクセスポリシーを追加

--- sns.tf
+++ sns.tf
@@ -1,3 +1,47 @@
 resource "aws_sns_topic" "cost_alert_topic" {
   name = "cost-alert-topic"
 }
+
+resource "aws_sns_topic_policy" "default" {
+  arn    = aws_sns_topic.cost_alert_topic.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:GetTopicAttributes",
+      "SNS:SetTopicAttributes",
+      "SNS:AddPermission",
+      "SNS:RemovePermission",
+      "SNS:DeleteTopic",
+      "SNS:Subscribe",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:Publish"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        "141864838114"
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["budgets.amazonaws.com"]
+    }
+
+    resources = [
+      aws_sns_topic.cost_alert_topic.arn
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
